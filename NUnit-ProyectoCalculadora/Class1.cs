bfc001a2c00c5f13296c443871fc08c5506ced27
ebalboa <erick.balboa.eb@gmail.com>
2017-09-15 09:27:51 -0400

    test nunit

---
+++ NUnit-ProyectoCalculadora/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ProyectoCalculadora.Controllers;
+
+namespace NUnit_ProyectoCalculadora
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestDivision()
+        {
+            var resultado = new Calculadora();
+            var dato= resultado.Division(4, 4);
+            Assert.AreEqual(0, dato);
+        }
+    }
+}
---
+++ ProyectoCalculadora/Controllers/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoCalculadora.Controllers
+{
+    public class Calculadora
+    {
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
